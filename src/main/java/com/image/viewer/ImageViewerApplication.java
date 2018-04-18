b5f2930c66986219da3e6d629daafc021f5ac4a0
mas_kh <mas_kh@i.ua>
2018-04-18 14:16:11 +0300

    Initial commit

---
+++ src/main/java/com/image/viewer/ImageViewerApplication.java
@@ -0,0 +1,12 @@
+package com.image.viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageViewerApplication.class, args);
+	}
+}
